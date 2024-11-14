e4e487faae2a0e8a54169e71785d35236805f7fe
md-hasan-akon1 <akonhasan680@gmail.com>
2024-11-14 09:22:08 +0600

    first commit

---
+++ src/app/module/auth/auth.route.ts
@@ -0,0 +1,41 @@
+
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AuthControllers } from './auth.controller';
+import { USER_ROLE } from '../../utils/constant';
+import auth from '../../middlewares/auth';
+
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  AuthControllers.loginUser,
+);
+
+router.post(
+  '/change-password',
+  auth(USER_ROLE.admin, USER_ROLE.user),
+  
+  AuthControllers.changePassword,
+);
+
+router.post(
+  '/refresh-token',
+  AuthControllers.refreshToken,
+);
+
+router.post(
+  '/forget-password',
+
+  AuthControllers.forgetPassword,
+);
+
+router.post(
+  '/reset-password',
+
+  AuthControllers.resetPassword,
+);
+
+export const AuthRoutes = router;
+
---
+++ src/app/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router  } from "express";
+import { UserRoutes } from "../module/users/user.routes";
+import { AuthRoutes } from "../module/auth/auth.route";
+
+
+const router = Router();
+const moduleRoutes=[
+    {   path: '/users',
+        route: UserRoutes
+    },
+    {   path: '/auth',
+        route: AuthRoutes
+    },
+ 
+]
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
