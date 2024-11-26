70b2d63783d935b278283c36fa30ae7043fb944a
md-hasan-akon1 <akonhasan680@gmail.com>
2024-11-26 14:36:09 +0600

    change file directory

--- src/app/helpers/fileUpload.ts
+++ src/app/helpers/fileUpload.ts
@@ -21,8 +21,10 @@ interface UploadResponse {
 // /var/www/uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-   cb(null, path.join(process.cwd(), "/var/www/uploads"));
-     //cb(null, path.join(process.cwd(), "uploads"));
+  //  cb(null, path.join(process.cwd(), "/var/www/uploads"));
+   cb(null, path.join("/var/www", "uploads"));
+
+    //  cb(null, path.join(process.cwd(), "/uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
--- src/app/module/users/user.controller.ts
+++ src/app/module/users/user.controller.ts
@@ -3,53 +3,51 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { userServices } from "./user.service";
 
-const createUser= catchAsync(async (req, res) => {
-  const query= req.query
- const result=await userServices.createUser(req.body,query) 
-  
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'User registered successfully. Please check your email for account activation.',
-      data: result,
-    });
+const createUser = catchAsync(async (req, res) => {
+  const query = req.query;
+  const result = await userServices.createUser(req.body, query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message:
+      "User registered successfully. Please check your email for account activation.",
+    data: result,
   });
+});
 
+const verifyOtp = catchAsync(async (req, res) => {
+  const result = await userServices.verifyOtp(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Otp verify successfully!",
+    data: result,
+  });
+});
+const updateUserProfile = catchAsync(async (req, res) => {
+  const result = await userServices.updateUserProfile(req);
+  console.log(req.file);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "update user profile successfully!",
+    data: result,
+  });
+});
+const getUserProfile = catchAsync(async (req, res) => {
+  const result = await userServices.getUserProfile(req.user);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "get user profile successfully!",
+    data: result,
+  });
+});
 
-  const verifyOtp = catchAsync(async (req, res) => {
-    const result = await userServices.verifyOtp(req.body);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Otp verify successfully!",
-      data: result,
-    });
-   
-  }); 
-  const updateUserProfile = catchAsync(async (req, res) => {
-    const result = await userServices.updateUserProfile(req);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "update user profile successfully!",
-      data: result,
-    });
-   
-  }); 
-  const getUserProfile = catchAsync(async (req, res) => {
-    const result = await userServices.getUserProfile(req.user);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "get user profile successfully!",
-      data: result,
-    });
-   
-  }); 
-
-  export const userController={
-    createUser,
-    verifyOtp,
-    getUserProfile,
-    updateUserProfile
-  }
+export const userController = {
+  createUser,
+  verifyOtp,
+  getUserProfile,
+  updateUserProfile,
+};
